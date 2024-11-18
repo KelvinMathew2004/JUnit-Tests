db0b004a90f3a7dd608070c09417cc5a6b02b768
Ted Lehr <t_l81@txstate.edu>
2024-11-18 13:31:18 -0600

    Removed unused fields in MiscOpPanel.java

--- app/src/main/java/calculator/panels/MiscOpPanel.java
+++ app/src/main/java/calculator/panels/MiscOpPanel.java
@@ -12,27 +12,16 @@ import calculator.utilities.UIColors;
 
 // this class has the C and f(x) operator Misc. removed from displaypanel
 public class MiscOpPanel extends JPanel {
-    private Calculator calculator;
-    private DisplayPanel displayPanel;
-    private OperatorHandler operatorHandler; //my ref to op handler
-    
-    // private JTextField displayField;
-    
-    // JPanel dpRightPanel = new JPanel(new GridLayout(1, 1, 10, 10));
-    
-    public MiscOpPanel(Calculator calculator, DisplayPanel displayPanel,OperatorHandler operatorHandler) {
-        this.calculator = calculator;
-        this.displayPanel = displayPanel;
-        this.operatorHandler = operatorHandler; // Initialize OperatorHandler
-        
+
+    public MiscOpPanel(Calculator calculator, DisplayPanel displayPanel, OperatorHandler operatorHandler) {
+
         setLayout(new GridLayout(1, 1, 10, 10));
-        String[] displayOps = {"C"};//"f(x)" to render graph
-        
-        
+        String[] displayOps = { "C" };// "f(x)" to render graph
+
         for (String op : displayOps) {
             CustomButton button = new CustomButton(op, UIColors.miscColor, UIColors.displayColor);
             button.setPreferredSize(new Dimension(190, 60));
-            /////////////test changes
+            ///////////// test changes
             button.setActionCommand(op); // Set the action command to the operation name
             button.addActionListener(operatorHandler);// Add the same OperatorHandler to handle all button presses
             this.add(button);
