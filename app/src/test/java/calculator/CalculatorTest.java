d39c2c8989e395b995df85f10f968c4ace03506b
Kelvin Mathew <dha25@txstate.edu>
2025-04-09 21:54:42 -0500

    Created the general structure of the CalculatorTest tests

--- app/src/test/java/calculator/CalculatorTest.java
+++ app/src/test/java/calculator/CalculatorTest.java
@@ -23,4 +23,40 @@ class CalculatorTest {
         assertEquals(8.5, classUnderTest.twoOpOperations());
     }
 
+    @Test
+    @DisplayName("Tests the addition function")
+    void testSimpleAddition() {
+        assertEquals(7, calc.evaluate("3+4"), 0.0001);
+    }
+
+    @Test
+    @DisplayName("Tests the subtraction function")
+    void testSimpleSubtraction() {
+        assertEquals(5, calc.evaluate("10-5"), 0.0001);
+    }
+
+    @Test
+    @DisplayName("Tests subtraction followed by addition")
+    void testTwoOpOperationsSubtractAdd() {
+        assertEquals(9, calc.evaluate("10-2+1"), 0.0001); // 10-2 = 8, 8+1 = 9
+    }
+
+    @Test
+    @DisplayName("Tests addition and multiplication order of operations")
+    void testTwoOpOperationsAddMultiply() {
+        assertEquals(20, calc.evaluate("2+3*6"), 0.0001); // 3*6 = 18, 18+2 = 20
+    }
+
+    @Test
+    @DisplayName("Tests the division function")
+    void testSimpleDivision() {
+        assertEquals(4, calc.evaluate("20/5"), 0.0001);
+    }
+
+    @Test
+    @DisplayName("Tests multiplication followed by division")
+    void testTwoOpOperationsMultiplyDivide() {
+        assertEquals(1.2, calc.evaluate("3*2/5"), 0.0001); // 3*2 = 6, 6/5 = 1.2
+    }
+
 }
